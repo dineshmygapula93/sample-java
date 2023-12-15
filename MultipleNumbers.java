de34d14c597723efa275e79b508e4046de87be62
dineshmygapula93 <153923310+dineshmygapula93@users.noreply.github.com>
2023-12-15 19:27:29 +0530

    Add files via upload

---
+++ MultipleNumbers.java
@@ -0,0 +1,10 @@
+public class MultipleNumbers {
+    public static void main(String args []){
+        //Declaring the variables
+        int a=5;
+        int b=10;
+        //Multiplication logic where * is the multiplication operator
+        int c=a*b;
+        System.out.println("The value :"+c);
+    }
+}
